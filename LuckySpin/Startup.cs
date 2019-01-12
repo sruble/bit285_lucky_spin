631c9170feb840f5a406eae88eccc7a9b083b81e
Brian Bansenauer <brianb@cascadia.edu>
2019-01-12 00:08:10 -0800

    Lucky7 service not registered

--- LuckySpin/Startup.cs
+++ LuckySpin/Startup.cs
@@ -15,7 +15,6 @@ namespace LuckySpin
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<Lucky7>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
